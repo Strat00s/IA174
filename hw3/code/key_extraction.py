93a2bc4cb926b31ccf00ad58184f41e1e4679147
Stratos <l.bastyr@seznam.cz>
2022-11-27 10:58:59 +0100

    comments

--- hw3/code/key_extraction.py
+++ hw3/code/key_extraction.py
@@ -2,6 +2,7 @@ import requests
 from hashlib import sha256
 from sage.all import *
 
+
 def APICall(difficulty: str, endpoint: str, uco:str, data: dict | None = None):
     url = f"https://ia174.fi.muni.cz/hw03/{difficulty}/{endpoint}/{uco}/"
     if data == None:
@@ -24,6 +25,7 @@ def isGenerator(point, curve):
         return True
     return False
 
+
 n1 = 940258296925944608662895221235664431210
 n2 = 42535295865117307932921825928971027169
 
@@ -37,6 +39,7 @@ pub_y = 0x39525bafcf33493141fd3f57d170f69af4ce41cd76ddb0f418781db2b4da5a8c
 gen_x = 0x19905ED22F9466CDF2FFC77877FD5A5519E11143FC9D691DED44ED6809AB4F50
 gen_y = 0x4AAC2A737CD8D1C15EE3D7C7F1B30F1E685599CB3C117012DF1AA67AC66AE384
 
+#create the curve and store its order and factors (without at least the last n2 factor, which is too large)
 ecurve  = EllipticCurve(GF(p), [a, b])
 order   = ecurve.order()
 factors = [x**y for (x, y) in order.factor()][:-1]  #don't use n2, as it is too large
@@ -46,25 +49,30 @@ print(f"  Order:   {order}")
 print(f"  Factors: {factors}")
 
 remainders = list()
+#generate remainders by calling api with points of small order (factor)
 for factor in factors:
     so_point = ecurve.point((0, 1, 0))
+
+    #generate points of specific order
     while so_point.order() != factor:
-        rng_point = ecurve.random_point()
-        so_point = rng_point * (order // factor)
+        rng_point = ecurve.random_point()   #practically guaranteed to be a generator
+        so_point  = rng_point * (order // factor)
 
     print(f"\nSmall order point: {so_point}")
     print(f"  Factor: {factor}")
     print(f"  Order:  {order}")
 
+    #get secret
     data = {"x": f"{hex(int(so_point[0]))}", "y": f"{hex(int(so_point[1]))}"}
     response = APICall("easy", "ecdh", "492875", data)
     if response == None:
         exit()
-
     secret = response["secret"]
-    original_so_point = so_point
+
+    original_so_point = so_point    #save copy for addition
     i = 0
     for i in range(0, factor):
+        #compress from ce.py
         x = int(so_point[0]).to_bytes(length=(p.bit_length() + 7) // 8, byteorder="big")
         y = int(so_point[1]).to_bytes(length=(p.bit_length() + 7) // 8, byteorder="big")
         if sha256(x + y).hexdigest() == secret:
@@ -72,12 +80,14 @@ for factor in factors:
         so_point += original_so_point   #iterate over point until secret match or infinity
     remainders.append((i + 1) % factor)
 
-print(f"Remainders: {remainders}")
+print(f"\nRemainders: {remainders}")
 
-generator   = ecurve.point((gen_x, gen_y))
-pubkey      = ecurve.point((pub_x, pub_y))
-key = crt(remainders, factors)
+#get the key using crt
+generator = ecurve.point((gen_x, gen_y))    #peters generator
+pubkey    = ecurve.point((pub_x, pub_y))    #peters public key
+key       = crt(remainders, factors)
 
+#keep adding product factors to key if not enough factors were used in the first place (moght not be needed when using all but last n2 factor)
 while generator * key != pubkey:
     key += prod(factors)
 
